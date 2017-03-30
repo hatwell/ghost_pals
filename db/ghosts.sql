dd8ed359179db30b675ff836ecd3d30bfb472cbf
hatwell <caroline.hatwell@gmail.com>
2017-03-30 15:58:54 +0100

    created sql file with first draft of data tables

---
+++ db/ghosts.sql
@@ -0,0 +1,52 @@
+CREATE TABLE location_types (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE ghost_types (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE services (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+)
+
+CREATE TABLE customers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE investigators (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE locations (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  location_type_id INT4 REFERENCES location_types(id)
+);
+
+CREATE TABLE ghosts (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  ghost_type_id INT4 REFERENCES ghost_types(id)
+);
+
+CREATE TABLE hauntings (
+  id SERIAL8 PRIMARY KEY,
+  haunting_date DATE,
+  ghost_id INT4 REFERENCES ghosts(id),
+  haunting_description TEXT
+);
+
+CREATE TABLE neghostiations (
+  id SERIAL8 PRIMARY KEY,
+  neghostiation_date DATE,
+  haunting_id INT4 REFERENCES haunting(id),
+  services_id INT4 REFERENCES services(id),
+  report TEXT,
+  successful BOOL
+);
