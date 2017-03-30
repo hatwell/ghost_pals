c92bbc3fbf786abb4bef777aa7ca99986f9126e1
hatwell <caroline.hatwell@gmail.com>
2017-03-30 16:03:00 +0100

    added DROP TABLE statements at top of sql file

--- db/ghosts.sql
+++ db/ghosts.sql
@@ -1,3 +1,14 @@
+DROP TABLE neghostiations;
+DROP TABLE hauntings;
+DROP TABLE ghosts;
+DROP TABLE locations;
+DROP TABLE investigators;
+DROP TABLE customers;
+DROP TABLE services;
+DROP TABLE ghost_types;
+DROP TABLE location_types;
+
+
 CREATE TABLE location_types (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
@@ -26,27 +37,27 @@ CREATE TABLE investigators (
 CREATE TABLE locations (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
-  location_type_id INT4 REFERENCES location_types(id)
+  location_type_id INT4 REFERENCES location_types(id) ON DELETE CASCADE
 );
 
 CREATE TABLE ghosts (
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255),
-  ghost_type_id INT4 REFERENCES ghost_types(id)
+  ghost_type_id INT4 REFERENCES ghost_types(id) ON DELETE CASCADE
 );
 
 CREATE TABLE hauntings (
   id SERIAL8 PRIMARY KEY,
   haunting_date DATE,
-  ghost_id INT4 REFERENCES ghosts(id),
+  ghost_id INT4 REFERENCES ghosts(id) ON DELETE CASCADE,
   haunting_description TEXT
 );
 
 CREATE TABLE neghostiations (
   id SERIAL8 PRIMARY KEY,
   neghostiation_date DATE,
-  haunting_id INT4 REFERENCES haunting(id),
-  services_id INT4 REFERENCES services(id),
+  haunting_id INT4 REFERENCES haunting(id) ON DELETE CASCADE,
+  services_id INT4 REFERENCES services(id) ON DELETE CASCADE,
   report TEXT,
   successful BOOL
 );
