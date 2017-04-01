88da1dc3ed96127ce4cf00d63f5b4ffae8537a54
hatwell <caroline.hatwell@gmail.com>
2017-04-01 22:21:42 +0100

    got mutliple controllers working

--- controllers/customers_controller.rb
+++ controllers/customers_controller.rb
@@ -6,3 +6,14 @@ get '/customers' do
   @customers = Customer.all()
   erb ( :"customers/index" )
 end
+
+get '/customers/new' do
+  erb( :"customers/new_customer" )
+end
+
+
+post '/customers/new' do
+  customer = Customer.new()
+  customer.save()
+  erb( :"hautings/new_haunting")
+end
