384dc79331868265a1ef5b4fff58254a8bcc6a55
hatwell <caroline.hatwell@gmail.com>
2017-04-12 16:55:24 +0100

    adding creds for pg gem

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql )
     begin
-      db = PG.connect({ dbname: 'db34eqo43348d5', host: 'postgres://dlvppddvkponnv:16b79ea377f1487372ecbb4474754aadb9ed010cb562732225e03aeb4f9700a5@ec2-54-247-92-185.eu-west-1.compute.amazonaws.com:5432/db34eqo43348d5', port: 5432 })
+      db = PG.connect({ dbname: 'db34eqo43348d5', host: 'ec2-54-247-92-185.eu-west-1.compute.amazonaws.com', port: 5432, user: "dlvppddvkponnv", password:"16b79ea377f1487372ecb" })
       result = db.exec( sql )
     rescue PG::ERROR => e
       puts "***********"
