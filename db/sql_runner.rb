ad9e60180631e1462ad38e7f0df4309d18696334
hatwell <caroline.hatwell@gmail.com>
2017-04-12 13:13:48 +0100

    added postgres db url to sql_runner file

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql )
     begin
-      db = PG.connect({ dbname: 'ghosts', host: 'localhost' })
+      db = PG.connect({ dbname: 'ghosts', host: 'postgres://dlvppddvkponnv:16b79ea377f1487372ecbb4474754aadb9ed010cb562732225e03aeb4f9700a5@ec2-54-247-92-185.eu-west-1.compute.amazonaws.com:5432/db34eqo43348d5' })
       result = db.exec( sql )
     ensure
       db.close
