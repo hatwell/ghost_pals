90091f987cb3feb9479df418d039eabe73b7106b
hatwell <caroline.hatwell@gmail.com>
2017-04-12 16:57:41 +0100

    changing PG creds to string

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql )
     begin
-      db = PG.connect({ dbname: 'db34eqo43348d5', host: 'ec2-54-247-92-185.eu-west-1.compute.amazonaws.com', port: 5432, user: "dlvppddvkponnv", password:"16b79ea377f1487372ecb" })
+      db = PG.connect("dbname=db34eqo43348d5 host=ec2-54-247-92-185.eu-west-1.compute.amazonaws.com port=5432 user=dlvppddvkponnv password=16b79ea377f1487372ecbb4474754aadb9ed010cb562732225e03aeb4f9700a5 sslmode=require")
       result = db.exec( sql )
     rescue PG::ERROR => e
       puts "***********"
